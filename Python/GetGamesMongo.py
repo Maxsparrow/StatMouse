20d380d0aaefb29acdb7cdd5e252cc185c47b1a7
Maxsparrow <Maxsparrow@gmail.com>
2015-01-01 10:11:58 -0600

    working on python code

--- Python/GetGamesMongo.py
+++ Python/GetGamesMongo.py
@@ -1,10 +1,41 @@
 import mysql.connector
+import random
 from Connections import *
 
-sdb = mysql.connector.connect(host=SQLhost,user=SQLuser,passwd=SQLpass,db="statmous_gamedata")
+def getsummonerids(amount=10):
 
-def getsummonerids(amount):
-    return False
+    attemptcount = 0
+    while attemptcount <= 5:
+        try:
+            sdb = mysql.connector.connect(host=SQLhost,user=SQLuser,passwd=SQLpass,db="statmous_gamedata")
+            cursor = sdb.cursor()
+
+            query = ("SELECT summonerId FROM summoners LIMIT 10000")
+
+            cursor.execute(query)
+
+            results = cursor.fetchall()
+            break
+        except mysql.connector.Error as errorno:
+            attemptcount += 1
+            sdb.close()
+            print errorno
+
+    summonerids = []
+
+    for summonerid in results:
+        summonerids.append(summonerid[0])
+
+    cursor.close()
+    sdb.close()
+
+    random.shuffle(summonerids)
+    summonerids = summonerids[0:amount]
+
+    return summonerids
+
+print getsummonerids()
+
 
 #from pymongo import MongoClient
 #mdb = MongoClient()
