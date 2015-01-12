a5edd1415cc7ee93f7ef0301f1efdcae75648a8a
Maxsparrow <maxsparrow@gmail.com>
2015-01-11 21:33:57 -0600

    First attempt at RMongo query

---
+++ R/ChampionAnalysisMongo.R
@@ -0,0 +1,7 @@
+library(RMongo)
+
+champion<-"Ezreal"
+mongocon<-RMongo::mongoDbConnect('games',host='localhost',port=27017)
+query = c("{$match:{'participants.championId':81}}",
+          "{$limit:1}")
+champgames<-RMongo::dbAggregate(mongocon,"games",query)
