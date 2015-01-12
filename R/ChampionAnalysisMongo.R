679471651f15e546066aa29a543b55b48217a223
Maxsparrow <maxsparrow@gmail.com>
2015-01-11 22:37:03 -0600

    Messing with RMongo more

--- R/ChampionAnalysisMongo.R
+++ R/ChampionAnalysisMongo.R
@@ -2,6 +2,9 @@ library(RMongo)
 
 champion<-"Ezreal"
 mongocon<-RMongo::mongoDbConnect('games',host='localhost',port=27017)
-query = c("{$match:{'participants.championId':81}}",
-          "{$limit:1}")
-champgames<-RMongo::dbAggregate(mongocon,"games",query)
+#query = c("{$match:{'participants.championId':81}}",
+#          "{$limit:1}")
+#champgames<-RMongo::dbAggregate(mongocon,"games",query)
+
+query = "{'participants.championId':81}"
+champgames<-RMongo::dbGetQuery(mongocon,"games",query,skip=0,limit=1)
