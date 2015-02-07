65fd3367fce515fd45b3159e5125ef41734c7d15
Maxsparrow <maxsparrow@gmail.com>
2015-02-06 21:20:36 -0600

    Trying to parse data differently for better cluster analysis

--- R/ChampionAnalysisMongo.R
+++ R/ChampionAnalysisMongo.R
@@ -10,7 +10,7 @@ makeparseddata<- function(championId,setlimit) {
     rawgamespull<-RMongo::dbGetQuery(mongocon,"parsed",query,skip=0,limit=setlimit)
     
     gamedata<-data.frame()
-    fullitemframe<-data.frame()
+    itemframe<-data.frame()
     
     ##Currently cost 973 seconds for 10000 games. I think the issue is we hit memory limits
     for(row in 1:nrow(rawgamespull)) {  
@@ -21,21 +21,47 @@ makeparseddata<- function(championId,setlimit) {
         ##For now, only keep first item count of each item to make analysis simpler
         itemlist<-itemlist[grepl("_1$",names(itemlist))]
         
+        itemcomboframe<-data.frame()
+        ##Reinventing this so that it shows item combinations for each order, so the orders are the columns, not the items
+        for(order in 0:30) {
+            itemcombo<-vector()
+            ##Find the itemIds that match the order we are on
+            for(itemnum in 1:length(itemlist)) {
+                if(itemlist[itemnum]==order) {
+                    itemcombo<-c(itemcombo,names(itemlist[itemnum]))
+                }
+            }
+            ##If there are any for this order, convert them and paste them together
+            ##Add them with the order to a data frame
+            if(length(itemcombo)!=0){
+                itemcombo<-substr(itemcombo,1,4)
+                itemcombo<-itemcombo[order(itemcombo)]
+                itemcombo<-paste0(itemcombo,collapse=";")
+                itemcomboframe<-rbind(itemcomboframe,data.frame(order,itemcombo))    
+            }
+        }
+        ##Transform the data frame we just got so the orders are the column names
+        itemcombos<-as.character(itemcomboframe$itemcombo)
+        itemcombos<-data.frame(t(itemcombos))
+        names(itemcombos)<-itemcomboframe$order
+        
+        ##Add this row's game data to full game data frame
         newrow<-subset(rawgamespull[row,],select=c(-items,-X_id))
-        gamedata<-rbind(gamedata,newrow)
-        fullitemframe<-rbind.fill(fullitemframe,data.frame(itemlist))
+        newrow<-cbind(newrow,itemcombos)
+        gamedata<-rbind.fill(gamedata,newrow)
+        
+        ##Add this row's items to a separate itemframe for clustering
+        itemframe<-rbind.fill(itemframe,data.frame(itemlist))
     }
     
     ##Setting winner as a number factor, seems more intuitive
     gamedata$winner<-as.factor(as.numeric(as.logical(gamedata$winner)))
-
-    champgames<-list(gamedata=gamedata,itemframe=fullitemframe)
+    champgames<-list(gamedata=gamedata,itemframe=itemframe)
     return(champgames)
 }
 
-addclusters <- function(champgames,numclusters=5) {
+addclusters <- function(champgames,numclusters=3) {
     champgames$itemframe[is.na(champgames$itemframe)]<-100
-    
     fitcl<-kmeans(champgames$itemframe,numclusters)
     ##Consider trying dbscan. Had issues in first attempts
     #library(fpc)
@@ -46,9 +72,12 @@ addclusters <- function(champgames,numclusters=5) {
     return(champgames)
 }
     
-clusteranalysis<-function(champgames,method) {
+##Don't want to use this anymore
+clusteranalysismedians<-function(champgames) {
     numclusters<-max(champgames$itemframe$cluster)
     ##TODO: Find a way to analyze clusters and pull out the most common holistic build, fairly difficult to assess
+    ##On to something with the medians method. Perhaps I need to just find a way to include higher order, late game items
+    ##Maybe if I loop through all the orders, find the most frequent items for each order over 10%, remove duplicates as we go along
     
     buildorderframe<-data.frame()
     for(cluster in unique(champgames$itemframe$cluster)) {
@@ -76,6 +105,17 @@ clusteranalysis<-function(champgames,method) {
     buildorderframe<-buildorderframe[with(buildorderframe,order(build,order,itemId)),c("build","order","itemId","itemName")]    
     
     return(buildorderframe)
+}
+
+clusteranalysiscombos<-function(champgames) {
+    clusterset<-champgames$gamedata[champgames$gamedata$cluster==1,]
+    n<-nrow(clusterset)
+    for(col in 17:ncol(clusterset)) {
+        comboperc<-table(clusterset[,col])/n
+        comboperc<-head(comboperc[order(-comboperc)])
+        print(paste0("Order",colnames(clusterset[col])))
+        print(comboperc)
+    }
 }
 
 rankclusters<-function(champgames) {
@@ -106,7 +146,7 @@ analyzechampions <- function(championName) {
     for(id in championIds) {
         champgames<-makeparseddata(id,1000)
         champgames<-addclusters(champgames,3)
-        buildorderframe<-clusteranalysis(champgames)
+        buildorderframe<-clusteranalysismedians(champgames)
         clusterscores<-rankclusters(champgames)
         
         ##Add cluster scores
